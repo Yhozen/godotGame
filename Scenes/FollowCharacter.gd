cf3b573b1bae9266969d32b8ef9fd36df1cc2fc4
Gabriel Pérez <gabriel@garox.org>
2020-02-11 21:45:18 -0300

    enemies follow character

--- Scenes/FollowCharacter.gd
+++ Scenes/FollowCharacter.gd
@@ -1,22 +1,21 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 onready var nav2D  = $Navigation2D
 onready var character = $KinematicBody2D 
-onready var enemy = $green_light
+onready var enemies = $Enemies
 
+onready var _timer = Timer.new()
+const TIME = 1.0/10
 
-
-
-func nose():
-	var path = nav2D.get_simple_path(enemy.global_position, character.global_position)
-	enemy.path = path
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func enemiesFollowPlayer():
+	for enemy in enemies.get_children():
+		var path = nav2D.get_simple_path(enemy.global_position, character.global_position)
+		enemy.path = path
+	
 func _ready():
-	nose()
+	add_child(_timer)
+	_timer.connect("timeout", self, "enemiesFollowPlayer")
+	_timer.set_wait_time(TIME)
+	_timer.set_one_shot(false) # Make sure it loops
+	_timer.start()
